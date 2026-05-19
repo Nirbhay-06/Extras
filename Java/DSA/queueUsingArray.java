77634c1e5b53d39e8c215d795f84f9c4af7c0beb
Nirbhay-06 <nirbhaypatel8511@gmail.com>
2026-05-19 09:06:23 +0530

    Queue Code Completed

--- Java/DSA/queueUsingArray.java
+++ Java/DSA/queueUsingArray.java
@@ -10,12 +10,13 @@ public class queueUsingArray {
 
     queueUsingArray(int size){
         Queue = new int[size];
+        capacity = size - 1;
         front = -1;
         rear = -1;
     }
 
     void insert(int ele){
-        if(rear == capacity){
+        if(rear == capacity ){
             System.out.println("Queue is Overflow.");
             return;
         }
@@ -46,6 +47,42 @@ public class queueUsingArray {
         }
     }
 
+    void display(){
+        if(front == -1 | rear == -1){
+            System.out.println("The Queue is Empty!!");
+            return;
+        }
+        else{
+            System.out.print("Elements in the Queue are(front --> rear): ");
+            int start = front;
+            for(int i = start; i<= rear; i++){
+                System.out.printf("%d -> ",Queue[i]);
+
+            }
+            System.out.println("Null");
+        }
+    }
+
+    void search(int target){
+        for(int i = front; i<=rear ; i++){
+            if(target == Queue[i]){
+                System.out.printf("Element found at position %d\n",i);
+                return;
+            }
+        }
+        System.out.println("Element not found.");
+    }
+
+    void isEmpty(){
+        if(front == -1 | rear == -1){
+            System.out.println("The Queue is Empty!!");
+            return;
+        }
+        else{
+            System.out.println("Queue is not empty!!");
+        }
+    }
+
 
     public static void main(String args[]){
         Scanner read = new Scanner(System.in);
@@ -54,59 +91,53 @@ public class queueUsingArray {
         int size = read.nextInt();
 
         queueUsingArray queue = new queueUsingArray(size);
+        loop1:
+        while(true){
+            int option = -1;
+            try{
+                System.out.printf("1. Insertion\n2. Deletion\n3. Transverse\n4. Search\n5. isEmpty\n0. Exit\nChoose an Option: ");
+                option = read.nextInt();
+            }
+            catch(InputMismatchException e){
+                System.out.println("Choose the number before the Operation.");
+                read.nextLine();
+                
+            }
 
-        int option = -1;
-        try{
-            System.out.printf("""
-            1. Insertion\n
-            2. Deletion\n
-            3. Transverse\n
-            4. Search\n
-            5. isEmpty\n
-            0. Exit\n
-            Choose an Option:     
-            """);
-            option = read.nextInt();
-        }
-        catch(InputMismatchException e){
-            System.out.println("Choose the number before the Operation.");
-            read.nextLine();
-               
-        }
+            switch(option){
+                case 0:
+                    System.out.println("Exiting..................");
+                    break loop1;
 
-        switch(option){
-            case 0:
-                System.out.println("Exiting..................");
-                break;
+                case 1:
+                    System.out.print("Enter an element to Insert: ");
+                    int element = read.nextInt();
+                    queue.insert(element);
+                    break;
+                
+                case 2:
+                    queue.delete();
+                    break;
 
-            case 1:
-                System.out.print("Enter an element to Insert: ");
-                int element = read.nextInt();
-                queue.insert(element);
-                break;
-            
-            case 2:
-                queue.delete();
-                break;
+                case 3:
+                    queue.display();
+                    break;
 
-            case 3:
-                queue.display();
-                break;
+                case 4:
+                    System.out.println("Enter an element to search: ");
+                    int target = read.nextInt();
+                    queue.search(target);
+                    break;
+                
 
-            case 4:
-                System.out.println("Enter an element to search: ");
-                int element = read.nextInt();
-                queue.search(element);
-                break;
-            
+                case 5:
+                    queue.isEmpty();
+                    break;
 
-            case 5:
-                queue.isEmpty();
-                break;
-
-            default:
-                System.out.println("Option Invalid");
-                break;
+                default:
+                    System.out.println("Option Invalid");
+                    break;
+            }
         }
 
         read.close();
